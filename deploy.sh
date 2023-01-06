f93c4fec969a5aaccf50a7118539e4b86702b6ac
linminjie <649922896@qq.com>
2023-01-07 00:05:37 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:cfmj/albion-doc.git master:gh-pages
+git push -f git@github.com:cfmj/albion-doc.git main:gh-pages
 
 cd -
